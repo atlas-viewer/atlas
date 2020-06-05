a8efa3145e440aa23a6a384e87e2d0ec4c1b3d3d
Stephen Fraser <stephen.fraser@digirati.com>
2020-06-05 14:15:15 +0100

    Updated rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import replace from '@rollup/plugin-replace';
+import compiler from '@ampproject/rollup-plugin-closure-compiler';
 import pkg from './package.json';
 
 export default [
@@ -13,13 +15,18 @@ export default [
         file: pkg.web,
         name: 'Atlas',
         format: 'umd',
+        sourcemap: true,
       },
     ],
     plugins: [
       typescript({ target: 'es5' }),
       resolve(), // so Rollup can find `ms`
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
       commonjs({ extensions: ['.js', '.ts'] }), // the ".ts" extension is required
       terser(),
+      compiler(),
     ],
   },
   {
@@ -37,6 +44,9 @@ export default [
     external: ['@atlas-viewer/dna'],
     plugins: [
       typescript(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
       resolve(), // so Rollup can find `ms`
       commonjs({ extensions: ['.js', '.ts'] }), // the ".ts" extension is required
       visualizer(),
