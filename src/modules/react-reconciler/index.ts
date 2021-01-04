2a91883fb568bfbd65c9d9ea223fcf20ccf343f0
Stephen Fraser <stephen.fraser@digirati.com>
2021-01-04 11:03:49 +0000

    Fixed exports

--- src/modules/react-reconciler/index.ts
+++ src/modules/react-reconciler/index.ts
@@ -1,13 +1,25 @@
 export * from './Atlas';
 export * from './reconciler';
+// Components
 export * from './components/AtlasAuto';
+export * from './components/AtlasContext';
 export * from './components/BoxDraw';
 export * from './components/HTMLPortal';
 export * from './components/RegionHighlight';
 export * from './components/ResizeWorldItem';
 export * from './components/TileSet';
-export * from './hooks/use-world-event';
+// Hooks
+export * from './hooks/use-after-frame';
+export * from './hooks/use-after-paint';
+export * from './hooks/use-atlas';
+export * from './hooks/use-before-frame';
+export * from './hooks/use-canvas';
 export * from './hooks/use-controlled-annotation-list';
+export * from './hooks/use-frame';
 export * from './hooks/use-mode';
 export * from './hooks/use-resize-world-item';
+export * from './hooks/use-runtime';
+export * from './hooks/use-world-event';
+// Other
+export * from './utility/can-drag';
 export * from './types';
