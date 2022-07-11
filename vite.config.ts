f3216ae1eeb516c78cd7c7693b1974721e628985
Stephen Fraser <stephen.fraser@digirati.com>
2022-07-11 20:37:09 +0100

    Removed UMD build for now

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'Atlas',
+      formats: ['es'],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
