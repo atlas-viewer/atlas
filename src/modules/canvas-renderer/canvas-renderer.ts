cac755968bc079acdf34f19e27e66b7b95b21448
Adam Brin <abrin@getty.edu>
2024-04-01 11:24:35 -0700

    back down

--- src/modules/canvas-renderer/canvas-renderer.ts
+++ src/modules/canvas-renderer/canvas-renderer.ts
@@ -484,8 +484,8 @@ export class CanvasRenderer implements Renderer {
               paint.display.points[index * 5 + 4] - paint.display.points[index * 5 + 2],
               x,
               y,
-              width + Number.MIN_VALUE + 1,
-              height + Number.MIN_VALUE + 1
+              width + Number.MIN_VALUE,
+              height + Number.MIN_VALUE
             );
           }
         }
